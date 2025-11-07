0d15c75e25862f71cc39ae88e116213b71888d64
Mariano Tepper <mariano.tepper@datastax.com>
2025-11-07 05:16:42 -0800

    Minor improvements to the dataset module

--- datasets/downloader.py
+++ datasets/downloader.py
@@ -7,4 +7,4 @@ def get_file(filename, destination_filename, bucket_name='astra-vector', region=
     s3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED),
                              region_name=region)
     with open(destination_filename, 'wb') as f:
-        s3_client.download_fileobj(bucket_name, f'wikipedia_squad/{filename}', f)
+        s3_client.download_fileobj(bucket_name, filename, f)
